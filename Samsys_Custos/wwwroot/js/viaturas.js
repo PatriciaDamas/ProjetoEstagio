bdba6183e6d173695a8fcca7bc724426609514db
Muppet57 <jonnhy157@gmail.com>
2018-05-24 10:54:49 +0100

    Refresh

--- Samsys_Custos/wwwroot/js/viaturas.js
+++
@@ -1,8 +0,0 @@
-﻿    var currentdate = new Date();
-var datetime = "Last Sync: " + currentdate.getDate() + "/"
-    + (currentdate.getMonth() + 1) + "/"
-    + currentdate.getFullYear() + " @ "
-    + currentdate.getHours() + ":"
-    + currentdate.getMinutes();
-    console.log(datetime);
-    document.getElementById("currentdate").value = datetime;
