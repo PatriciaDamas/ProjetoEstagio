e39a86d573cb2690bdf67df033d97bdfa1f85fb5
ticha7 <claudiadamas27@gmail.com>
2018-05-29 17:10:45 +0100

    custos - atualizações

--- Samsys_Custos/Samsys_Custos/wwwroot/js/cascading_select.js
+++ Samsys_Custos/Samsys_Custos/wwwroot/js/cascading_select.js
@@ -1,21 +1,22 @@
 ﻿
         $(document).ready(function () {
-            $(document.body).on('change', '#tipo_categoria', function () {
+            $(document.body).on('change', '.tipo_categoria', function () {
 
-                $("#rubrica").empty();
+                $(".rubrica").empty();
                 $.ajax({
                     type: 'POST',
                     url: 'https://localhost:44339/CUSTO/getrubrica',
                     dataType: 'json',
-                    data: { id: $("#tipo_categoria").val() },
+                    data: { id: $(".tipo_categoria").val() },
 
 
                     success: function (data) {
                         var myarray = JSON.parse(data)
                         $.each(myarray, function (i, subcategory) {
 
-                            $("#rubrica").append('<option value="' + subcategory.Value + '">' +
+                            $(".rubrica").append('<option value="' + subcategory.Value + '">' +
                                 subcategory.Text + '</option>');
+                            console.log(subcategory.Value)
 
                         });
                     },
