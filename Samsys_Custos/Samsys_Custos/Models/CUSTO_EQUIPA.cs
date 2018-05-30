592c2189fb848f2b17f891704f504c631fda4a0b
Muppet57 <jonnhy157@gmail.com>
2018-05-30 10:04:24 +0100

    Model View

---
+++ Samsys_Custos/Samsys_Custos/Models/CUSTO_EQUIPA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Samsys_Custos.Models
+{
+    public class CUSTO_EQUIPA
+    {
+        public string Equipas { get; set; }
+        public int Total{ get; set; }
+        public string mes { get; set; }
+    }
+}
