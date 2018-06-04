bfd2f7fb02d728a151d7ce69180a2a4717277cb9
Muppet57 <jonnhy157@gmail.com>
2018-06-04 10:29:06 +0100

    Teste gráfico.v1

---
+++ Samsys_Custos/Samsys_Custos/wwwroot/js/charts.js
@@ -0,0 +1,11 @@
+﻿$("document").ready(function () {
+    let saveData = $.ajax({
+        type: 'get',
+        url: 'https://localhost:44363/CUSTO/GeralJson',
+        dataType: "json",
+        success: function (data) {
+            var parseddata = JSON.parse(data);
+            console.log(data);
+        }
+    });
+})
