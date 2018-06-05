8a8269468e588af5a86ccb521f6dcb6ef842e3bc
Muppet57 <jonnhy157@gmail.com>
2018-06-05 18:49:12 +0100

    Erro Filtro gráfico corrigido

--- Samsys_Custos/Samsys_Custos/wwwroot/js/charts.js
+++ Samsys_Custos/Samsys_Custos/wwwroot/js/charts.js
@@ -6,86 +6,94 @@
             dataType: "json",
             data: { ano: $("#select_ano").val() },
             success: function (data) {
-                var dataprovider = new Array();
-                var meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-                var keys;
-                var ano = $("#select_ano").val();
+                if (!jQuery.isEmptyObject(data)) {
 
-                
-                for (var j = 0; j < meses.length; j++) {
+                    var dataprovider = new Array();
+                    var meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+                    var keys;
+                    var ano = $("#select_ano").val();
 
-                    var tempdata = {};
-                    Object.assign(tempdata, {
-                        "Mês": meses[j]
-                    });
 
-                    for (var i = 0; i < data.length; i++) {
-                        if (data[i].mes.toLowerCase() === meses[j].toLowerCase()) {
-                            var Categoria = data[i].nomeCompleto;
-                            var Total = data[i].total;
-                            Object.assign(tempdata, {
-                                [Categoria]: Total
-                            });
+                    for (var j = 0; j < meses.length; j++) {
+
+                        var tempdata = {};
+                        Object.assign(tempdata, {
+                            "Mês": meses[j]
+                        });
+
+                        for (var i = 0; i < data.length; i++) {
+                            if (data[i].mes.toLowerCase() === meses[j].toLowerCase()) {
+                                var Categoria = data[i].nomeCompleto;
+                                var Total = data[i].total;
+                                Object.assign(tempdata, {
+                                    [Categoria]: Total
+                                });
+                            }
                         }
-                    }
-                    if (Object.keys(tempdata).length > 1) {
-                        dataprovider.push(tempdata);
+                        if (Object.keys(tempdata).length > 1) {
+                            dataprovider.push(tempdata);
+                        }
+
                     }
 
+
+                    var settings = {
+                        "type": "serial",
+                        "theme": "light",
+                        "legend": {
+                            "horizontalGap": 10,
+                            "maxColumns": 1,
+                            "position": "right",
+                            "useGraphSettings": true,
+                            "markerSize": 10
+                        },
+                        "dataProvider": [],
+                        "valueAxes": [{
+                            "stackType": "regular",
+                            "axisAlpha": 1,
+                            "gridAlpha": 0
+                        }],
+                        "graphs": [],
+                        "categoryField": "Mês",
+                        "categoryAxis": {
+                            "gridPosition": "start",
+                            "axisAlpha": 0,
+                            "gridAlpha": 0,
+                            "position": "left"
+                        },
+                        "export": {
+                            "enabled": true
+                        }
+
+                    };
+
+
+                    keys = Object.keys(dataprovider[0]);
+
+                    for (var k = 1; k < keys.length; k++) {
+                        settings.graphs.push({
+                            "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+                            "fillAlphas": 1,
+                            "id": "AmGraph-" + k,
+                            "title": keys[k],
+                            "labelText": "[[value]]€" + " - " + keys[k],
+                            "type": "column",
+                            "valueField": keys[k]
+                        });
+                    }
+                    settings.dataProvider = dataprovider;
+
+                    var chart = AmCharts.makeChart("chartdiv", settings);
+
+                    console.log(settings);
+
+
+                } else {
+                    alert("Não existem custos associados ao ano selecionado");
                 }
-
-
-                var settings = {
-                    "type": "serial",
-                    "theme": "light",
-                    "legend": {
-                        "horizontalGap": 10,
-                        "maxColumns": 1,
-                        "position": "right",
-                        "useGraphSettings": true,
-                        "markerSize": 10
-                    },
-                    "dataProvider": [],
-                    "valueAxes": [{
-                        "stackType": "regular",
-                        "axisAlpha": 1,
-                        "gridAlpha": 0
-                    }],
-                    "graphs": [],
-                    "categoryField": "Mês",
-                    "categoryAxis": {
-                        "gridPosition": "start",
-                        "axisAlpha": 0,
-                        "gridAlpha": 0,
-                        "position": "left"
-                    },
-                    "export": {
-                        "enabled": true
-                    }
-
-                };
-
-
-                keys = Object.keys(dataprovider[0]);
-
-                for (var k = 1; k < keys.length; k++) {
-                    settings.graphs.push({
-                        "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
-                        "fillAlphas": 1,
-                        "id": "AmGraph-" + k,
-                        "title": keys[k],
-                        "labelText":"[[value]]€" + " - " + keys[k], 
-                        "type": "column",
-                        "valueField": keys[k]
-                    });
-                }
-                settings.dataProvider = dataprovider;
-                var chart = AmCharts.makeChart("chartdiv", settings);
-
-                console.log(settings);
-
-               
-               
+            },
+            error: function(err){
+                console.log(err);
             }
         });
     }
