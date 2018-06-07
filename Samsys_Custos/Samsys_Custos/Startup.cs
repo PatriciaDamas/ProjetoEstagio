dc87c8c41bea819684659bb4247fcb56728f3289
JoaoGiesteira <34487467+JoaoGiesteira@users.noreply.github.com>
2018-06-07 18:19:16 +0100

    Versão Funcional

--- Samsys_Custos/Samsys_Custos/Startup.cs
+++ Samsys_Custos/Samsys_Custos/Startup.cs
@@ -71,7 +71,7 @@ namespace Samsys_Custos
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=CUSTO}/{action=grafico_gerais}/{id?}");
             });
         }
     }
