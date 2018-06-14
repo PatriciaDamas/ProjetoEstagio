329241d8e0da7fcd27802d2ff34e775cf4d4132c
Muppet57 <jonnhy157@gmail.com>
2018-06-15 00:55:34 +0100

    Paginas Relacionadas com utilizadores adicionadas ao projeto

---
+++ Samsys_Custos/Samsys_Custos/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Samsys_Custos.Data;
+using Samsys_Custos.Models;
+
+[assembly: HostingStartup(typeof(Samsys_Custos.Areas.Identity.IdentityHostingStartup))]
+namespace Samsys_Custos.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
