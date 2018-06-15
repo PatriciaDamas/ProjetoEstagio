3cd249d604c46a8782d9476057cb93ba651cbb4c
Patricia <claudiadamas27@gmail.com>
2018-06-15 14:53:26 +0100

    Correcção das tabelas/Criar: Atribuições Gerais Atribuições Viaturas Atribuições GSM Fornecedores Inserção de Icon Estelização de botões (Atribuições)

--- Samsys_Custos/Samsys_Custos/wwwroot/js/site.js
+++ Samsys_Custos/Samsys_Custos/wwwroot/js/site.js
@@ -1,4 +1,9 @@
 ﻿// Write your JavaScript code.
 $(document).ready(function () {
-
+    $("#btnCriarGsm").click(function () {
+        location.href = "https://localhost:44382/ATRIBUICAO/CreateGSM"
+    });
+    $("#btnCriarViatura").click(function () {
+        location.href = "https://localhost:44382/ATRIBUICAO/CreateViatura"
+    });
 });
