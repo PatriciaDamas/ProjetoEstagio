31d6ab68741555126605e440798fa94d0d5f6f12
Patricia <claudiadamas27@gmail.com>
2018-06-21 19:16:38 +0100

    Tabela bd MEDIA

--- Samsys_Custos/Samsys_Custos/Data/Migrations/20180621181159_MEDIA_CUSTOS.Designer.cs
+++ Samsys_Custos/Samsys_Custos/Data/Migrations/20180621181159_MEDIA_CUSTOS.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Samsys_Custos.Data;
 
 namespace Samsys_Custos.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180621181159_MEDIA_CUSTOS")]
+    partial class MEDIA_CUSTOS
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Samsys_Custos/Samsys_Custos/Models/MEDIA_CUSTOS.cs
@@ -0,0 +1,28 @@
+﻿using Samsys_Custos.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Samsys_Custos.Models
+{
+    public class MEDIA_CUSTOS
+    {
+        [Key]
+        public int id_media { get; set; }
+
+
+        [ForeignKey("EQUIPA")]
+        public string id_equipa { get; set; }
+        public EQUIPA EQUIPA { get; set; }
+
+
+        public string nome { get; set; }
+        public int qtd_colaboradores { get; set; }
+        public decimal Total { get; set; }
+        public string mes { get; set; }
+        public string ano { get; set; }
+    }
+}
