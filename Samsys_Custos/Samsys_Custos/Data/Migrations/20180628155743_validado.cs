ed1797cfb4123e7c935788935ff48f39e98a723e
Muppet57 <jonnhy157@gmail.com>
2018-06-28 17:02:53 +0100

    Remoção de comentarios

---
+++ Samsys_Custos/Samsys_Custos/Data/Migrations/20180628155743_validado.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Samsys_Custos.Data.Migrations
+{
+    public partial class validado : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "validado",
+                table: "DADOS_PHC",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "validado",
+                table: "DADOS_PHC");
+        }
+    }
+}
